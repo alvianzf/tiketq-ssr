ea08614d9054b5e798c1f308f9a463e50fcf3508
alvianzf <alvianzf@gmail.com>
2023-08-27 20:20:10 +0700

    deploy: add meta tags to handle unsecure http request

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <link
         rel="icon"
         href="/favicon.png"
